5b95dd0ac27fa4b14bbf3c9bd7c6cbf7cabdc015
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2024-09-26 21:30:51 +0300

    cat image and various icons were added to login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="">
-    <form class="flex flex-col justify-center items-center gap-y-3" @submit.prevent="login">
-      <InputText type="text" v-model="username" placeholder="Kullanıcı Adı" />
-      <Password v-model="password" placeholder="Şifre" :feedback="false" />
-      <Button type="submit" label="Giriş Yap" icon="pi pi-check" />
+  <div class="min-h-screen w-full space-y-3 flex flex-col justify-center items-center">
+    <form class="flex flex-col justify-center items-center gap-y-3 border border-green-500 rounded-xl p-5"
+      @submit.prevent="login">
+      <img class="w-56 h-auto rounded-lg object-contain" src="~/assets/images/cat-login.jpg" alt="Kedi görseli">
+
+      <h2 class="text-xl mb-3">Hesabınıza Giriş Yapın</h2>
+
+      <IconField>
+        <InputIcon class="pi pi-user" />
+        <InputText type="text" v-model="username" placeholder="Kullanıcı Adı" />
+      </IconField>
+
+      <IconField>
+        <InputIcon class="pi pi-key" />
+        <InputText v-model="password" placeholder="Şifre" type="password" />
+      </IconField>
+
+      <Button type="submit" label="Giriş Yap" icon="pi pi-check" class="mt-3" />
     </form>
-    <div v-if="errorMessage">{{ errorMessage }}</div>
+    <div v-if="errorMessage" class="text-red-500 font-medium">{{ errorMessage }}</div>
   </div>
 </template>
 
